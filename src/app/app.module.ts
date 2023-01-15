9a66eedbacaedc7bcaf00290e4b31817634424ca
iodaniel <iodaniel@icloud.com>
2023-01-14 17:00:58 -0700

    week-02

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ContactListComponent } from './contacts/contact-list/contact-list.component';
+import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContactsComponent,
+    ContactListComponent,
+    ContactDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Contact } from '../contact.model';
 
 @Component({
   selector: 'app-contact-list',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-list.component.css']
 })
 export class ContactListComponent {
+  contacts: Contact[]=[
+    new Contact(
+      1,
+       'R. Kent Jackson',
+        'jacksonk@byui.edu',
+        '208-496-3771',
+         '../../assets/images/jacksonk.jpg'
+
+    ),
+    new Contact(
+      2,
+      "Rex Barzee",
+      "barzeer@byui.edu",
+       "208-496-3768",
+        "../../assets/images/barzeer.jpg",
+      )
+  ];
+  constructor(){}
+
+
+
+
+
+
 
 }
